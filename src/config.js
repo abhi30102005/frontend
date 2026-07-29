9472459601d6cafb9e7ada347c83c7517eb0b2f7
Abhishek Kumar Yadav <abhishekkumaryadav3010@gmail.com>
2026-07-29 17:53:28 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-REACT_APP_API_URL=https://backend-z5dv.onrender.com
-REACT_APP_DASHBOARD_URL=https://dashboard-nine-phi-81.vercel.app
+REACT_APP_API_URL="https://backend-z5dv.onrender.com"
+REACT_APP_DASHBOARD_URL="https://dashboard-nine-phi-81.vercel.app"
